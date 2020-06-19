2acaf089c3ebfdefed386b25aa35f122749176b8
Ollie Nye <ollie@nyemail.co.uk>
2020-06-19 01:42:10 +0100

    Add spec helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'bundler/setup'
+require 'ripple_token'
+
+RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = '.rspec_status'
+
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.disable_monkey_patching!
+
+  config.order = :random
+  config.color = true
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
