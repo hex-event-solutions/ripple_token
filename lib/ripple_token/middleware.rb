957d33ebc55f07565cfb9b4d448a7b38d4271e80
Ollie Nye <ollie@nyemail.co.uk>
2020-06-19 03:12:39 +0100

    Might help if app was called again

--- lib/ripple_token/middleware.rb
+++ lib/ripple_token/middleware.rb
@@ -14,28 +14,26 @@ module RippleToken
       method = env['REQUEST_METHOD']
       path = env['PATH_INFO']
 
-      if token.public_path? method, path
-        @app.call(env)
-      else
+      unless token.public_path? method, path
         encoded_token = env['HTTP_AUTHORIZATION']&.gsub(/^Bearer /, '') || ''
         decoded_token = token.decode(encoded_token)
-        pp decoded_token
         store_user_details(decoded_token, env)
       end
+      @app.call(env)
     end
 
     private
 
     def store_user_details(decoded_token, env)
-      # env['keycloak.token'] = decoded_token
-      # env['keycloak.user_id'] = decoded_token['sub']
-      # env['keycloak.user_roles'] = decoded_token['realm_access']['roles']
-      # env['keycloak.user_email'] = decoded_token['email']
-      # env['keycloak.user_name'] = decoded_token['name']
-      # env['keycloak.user_first_name'] = decoded_token['given_name']
-      # env['keycloak.user_last_name'] = decoded_token['family_name']
-      # env['keycloak.user_phone'] = decoded_token['phone']
-      # env['keycloak.user_email_verified'] = decoded_token['email_verified']
+      env['keycloak.token'] = decoded_token
+      env['keycloak.user_id'] = decoded_token['sub']
+      env['keycloak.user_roles'] = decoded_token['realm_access']['roles']
+      env['keycloak.user_email'] = decoded_token['email']
+      env['keycloak.user_name'] = decoded_token['name']
+      env['keycloak.user_first_name'] = decoded_token['given_name']
+      env['keycloak.user_last_name'] = decoded_token['family_name']
+      env['keycloak.user_phone'] = decoded_token['phone']
+      env['keycloak.user_email_verified'] = decoded_token['email_verified']
     end
 
     def token
